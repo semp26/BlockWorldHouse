528e0949093e984f57aa1380b7dfd1278a72ae79
semp26 <burkert.max@gmail.com>
2023-10-23 13:55:53 +0200

    f

--- Aufgabe 2 Testat Burkert.py
+++ Aufgabe 2 Testat Burkert.py
@@ -2,11 +2,11 @@ from pyblockworld import World
 
 
 class Wall:
-    def __init__(self, p_pos: tuple, p_bw: World):
+    def __init__(self, p_pos: tuple, p_bw: World, rotated: bool):
         self.width = 6
         self.height = 5
         self.pos = p_pos
-        self.rotated = False
+        self.rotated = rotated
         self.material_id = "default:stone"
         self.bw = p_bw
 
@@ -15,14 +15,15 @@ class Wall:
         if not self.rotated:
             self.bw.setBlocks(x + 1, y - 1, z + 1, x + self.width, y + self.height - 1, z + 1, self.material_id)
         else:
-            self.bw.setBlocks(x + 1, y - 1, z + 1, x + 1, y + self.height - 1, z + self.width + 2, self.material_id)
+            self.bw.setBlocks(x + 1, y - 1, z + 1, x + 1, y + self.height - 1, z + self.width, self.material_id)
 
 
 def b_key_pressed(p_world: World):
     x, y, z = p_world.player_position()
-    wand1 = Wall((x, y, z), p_world)
-    wand2 = Wall((x,y,z),)
-    W
+    wand1 = Wall((x, y, z), p_world, False)
+    wand2 = Wall((x, y, z), p_world, True)
+    wand1.build()
+    wand2.build()
 
 
 world = World()
